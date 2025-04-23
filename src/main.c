502211ad51e9947b41174ff3346e17dd0c5ee6a7
KrispyFbacon <fbrandaoporto@gmail.com>
2025-04-23 20:59:43 +0100

    main better organized

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yes <yes@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 15:04:38 by frbranda          #+#    #+#             */
-/*   Updated: 2025/04/23 18:05:26 by yes              ###   ########.fr       */
+/*   Updated: 2025/04/23 18:50:39 by yes              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,51 @@ int	only_spaces(char	*input)
 	return (TRUE);
 }
 
-void	ft_minishell(t_shell *shell)
+void	read_input(t_shell *shell)
+{
+	shell->prev_exit_status = shell->exit_status; // for signal exit_status
+	shell->exit_status = 0;
+	set_signo(0); // reset signo
+	errno = 0; // reset errno
+	shell->input = readline("minishell> ");
+	if (get_signo() == CTRL_C)
+		shell->prev_exit_status = CTRL_C + 128;
+	if (!shell->input)
+	{
+		ft_putstr_fd("exit\n", 2);
+		free_exit (shell, shell->prev_exit_status);
+	}
+	if (only_spaces(shell->input) == FALSE)
+		add_history(shell->input);
+}
+
+void	builtin_and_cmd(t_shell *shell)
 {
 	t_bt	func;
 
+	if (shell->args && shell->args[0])
+	{
+		func = ft_isbuiltin(shell->args[0], shell);
+		if (func)
+		{
+			if (func(shell->args, shell) != 0)
+				printf("Error executing %s\n", shell->args[0]);
+			// TODO if func export e mudou update env
+			//TODO envp arg to env list (FREE OLD ENV)
+		}
+		else
+		{
+			shell->exit_status = 1;
+			ft_printf_fd(2, "Command not found\n"); 
+		}
+	}
+}
+
+void	ft_minishell(t_shell *shell)
+{
 	while (1)
 	{
-		shell->prev_exit_status = shell->exit_status; // for signal exit_status
-		shell->exit_status = 0;
-		set_signo(0); // reset signo
-		errno = 0; // reset errno
-		shell->input = readline("minishell> ");
-		if (get_signo() == CTRL_C)
-			shell->prev_exit_status = CTRL_C + 128;
-		if (!shell->input)
-		{
-			ft_putstr_fd("exit\n", 2);
-			free_exit (shell, shell->prev_exit_status);
-		}
-		if (only_spaces(shell->input) == FALSE)
-			add_history(shell->input);
+		read_input(shell);
 		if (ft_parsing(shell, shell->input) != SUCCESS)
 		{
 			free_ref(&shell->input);
@@ -53,22 +78,7 @@ void	ft_minishell(t_shell *shell)
 		}
 		tokenizer(&shell, ft_strdup(shell->input));
 		shell->args = token_list_to_array(shell->token_list);
-		if (shell->args && shell->args[0])
-		{
-			func = ft_isbuiltin(shell->args[0], shell);
-			if (func)
-			{
-				if (func(shell->args, shell) != 0)
-					printf("Error executing %s\n", shell->args[0]);
-				// TODO if func export e mudou update env
-				//TODO envp arg to env list (FREE OLD ENV)
-			}
-			else
-			{
-				shell->exit_status = 1;
-				ft_printf_fd(2, "Command not found\n");
-			}
-		}
+		builtin_and_cmd(shell);
 		free_char_pp_ref(&shell->args);
 		free_tokens(&shell->token_list);
 		free_ref(&shell->input);
